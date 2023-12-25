a92df6fa02f878cd6a016e1cab3bf113058124e1
MacOne <mac@MacOnes-MacBook-Pro.local>
2023-12-25 12:04:33 +0700

    fix AI bugs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { CssBaseline } from '@mui/material';
 import { Route, Switch } from 'react-router-dom';
+
 import useStyles from './styles';
+import useAI from './AI';
 
-import { Actors, MovieInfor, MovieList, Movies, NavBar, Profile } from '.';
+import { Actors, MovieInfor, Movies, NavBar, Profile } from '.';
 
-// -> root -> all movies -> /id123 -> movie information -> more //
 const App = () => {
   const classes = useStyles();
+  const alanBtnContainer = useRef();
+
+  useAI();
+
   return (
     <div className={classes.root}>
       <CssBaseline />
@@ -18,24 +23,18 @@ const App = () => {
           <Route exact path="/movie/:id">
             <MovieInfor />
           </Route>
-
           <Route exact path="/actors/:id">
             <Actors />
           </Route>
-
           <Route exact path={['/', '/approved']}>
             <Movies />
           </Route>
-
-          <Route exact path="/movies">
-            <h1>Movies</h1>
-          </Route>
-
           <Route exact path="/profile/:id">
             <Profile />
           </Route>
         </Switch>
       </main>
+      <div ref={alanBtnContainer} />
     </div>
   );
 };
